301428ce46a05e5b13f6340ea6f4752721b75a22
Alex Villa <avilla0429@gmail.com>
2013-06-09 14:41:04 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,9 +28,17 @@ describe "UserPages" do
 			it "should not create a user" do
 				expect { click_button submit}.not_to change(User, :count)
 			end
+			
+			describe "after submission" do
+				before { click_button submit }
+				
+				it { should have_selector('title', text: 'Sign Up') }
+				it { should have_content('error') }
+			end
+			
 		end
 		
-		describe "with vlaid information" do
+		describe "with valid information" do
 			before do
 				fill_in "Name", 			with: "Example User"
 				fill_in "Email",			with: "user@example.com"
@@ -41,7 +49,16 @@ describe "UserPages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
+			
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by_email('user@example.com') }
+				
+				it { should have_selector('title', text:user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+				it { should have_link('Sign out') }
+			end
+			
 		end
 	end
-	
 end
